0a23adb5190b3c609e3e090b65fb693883c27cf8
游鱼惊梦 <youmeng2019@gmail.com>
2019-09-16 22:28:43 +0800

    21合成两个有序的链表-递归

---
+++ week7/21合成两个有序的链表-递归.cpp
@@ -0,0 +1,33 @@
+/**
+ *  @Author:游鱼惊梦
+ *  @Date: 2019/9/13
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        if (!l1)
+            return l2;
+        if (!l2)
+            return l1;
+        if (l1->val < l2->val){
+            l1->next = mergeTwoLists(l1->next,l2);
+            return l1;
+        } else{
+            l2->next = mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+    }
+};
