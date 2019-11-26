842f070bd4de0e6986ebb5b588cf961e187e8448
游鱼惊梦 <youmeng2019@gmail.com>
2019-11-26 22:25:53 +0800

    53最大子序列

---
+++ 动态规划/53最大子序列和.java
@@ -0,0 +1,18 @@
+
+
+class Solution {
+    int maxSubArray(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            //状态转移，以i为终点的最大子序列都依赖于i-1的状态
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
+            if (dp[i] > max){
+                max = dp[i];
+            }
+        }
+        return max;
+    }
+}
+
